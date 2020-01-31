415fe8fc23f7bebbc33df30ed7e6257368fe9272
Michelle Scott <scottmm374@gmail.com>
2020-01-31 10:54:01 -0500

    added pg

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,16 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 
   testing: {
     client: "sqlite3",
