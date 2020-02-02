e2a52316af6914daa0eca3a301237513bf500f03
Michelle Scott <scottmm374@gmail.com>
2020-02-02 12:40:43 -0500

    add child route, need to test heroku

--- api/routes/user/childRouter.js
+++ api/routes/user/childRouter.js
@@ -2,33 +2,27 @@ const express = require("express");
 const router = express.Router();
 const childMod = require("../../models/children-model.js");
 
-// * Works on LH
-//! Not on Heroku
-router.get("/", (req, res, next) => {
-  childMod
-    .findChildren()
-    .then(children => {
-      res.status(200).json(children);
-    })
-    .catch(err => {
-      console.log(err);
-      next();
-    });
+// * Works Get all children
+router.get("/", async (req, res, next) => {
+  try {
+    const children = await childMod.findChildren();
+    res.status(200).json(children);
+  } catch (err) {
+    console.log(err);
+    next();
+  }
 });
 
-// * Works on LH
-//! Not on Heroku
-router.get("/:id", (req, res, next) => {
-  const id = req.params.id;
-  childMod
-    .findChildById(id)
-    .then(child => {
-      res.status(200).json(child);
-    })
-    .catch(err => {
-      console.log(err);
-      next();
-    });
+// * Works Get Child by Childs id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const child = await childMod.findChildById(id);
+    res.status(200).json(child);
+  } catch (err) {
+    console.log(err);
+    next();
+  }
 });
 
 router.post("/addChild", (req, res, next) => {
